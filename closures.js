4b66fb8cd81d0c64918a17f49db6a7cc7106658d
Jerry Chen <raychen31638@gmail.com>
2020-01-07 15:14:40 -0600

    finished

--- closures.js
+++ closures.js
@@ -23,12 +23,13 @@ function outer() {
 */
   
 // Code Here
-
+let inner = outer();
 
 
 //Once you do that, invoke inner.
 
 //Code Here
+inner();
 
 
 
@@ -53,7 +54,8 @@ function callFriend(name) {
 
 //Code Here
 
-
+let callJake = callFriend('Jake');
+callJake('435-555-9248');
 
 ////////// PROBLEM 3 //////////
 
@@ -63,14 +65,19 @@ function callFriend(name) {
 
 //Code Here
 
-
+function makeCounter(){
+  var initVal = 0;
+  return function(){
+    return ++initVal;
+  }
+}
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,24 +94,32 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
+  var initVal = value;
 
   return {
+    inc: function(){
+      return ++initVal;
+    },
+    dec: function(){
+      return --initVal;
+    }
 
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
 ////////// PROBLEM 5 //////////
 
 /*
-  Inside the motivation function create another function called message that will return the welcome text with the firstname and lastname.
+  Inside the motivation function create another function called message 
+  that will return the welcome text with the firstname and lastname.
   The final message should say "You're doing awesome, keep it up firstname lastname." 
   (Hint: don't forget to have a space between the firstname and lastname and a period at the end of the sentence.)
 */
@@ -113,9 +128,12 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  function message(){
+    return `${welcomeText} ${firstname} ${lastname}.`;
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,10 +161,14 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
+    publicMethod: function(){
+      return privateMethod();
+    }
     // Code here.
   };
 })();
 
+module.publicMethod();
 
 
 ////////// PROBLEM 7 //////////
@@ -163,6 +185,14 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(num){
+      secret += num;
+      return secret;
+    },
+    takeAwayFromSecret: function(num){
+      secret -= num;
+      return secret;
+    }
   };
 }
 
@@ -180,17 +210,21 @@ function secretNumber() {
     4 seconds after call - log 4
     5 seconds after call - log 5
 
-  However, because each call to console.log occurs after the loop has finished, the value of i has changed before the console.log executes.
+  However, because each call to console.log occurs 
+  after the loop has finished, the value of i has changed before the console.log executes.
   We'll need to use a closure to preserve a reference to i at the time of execution.
   
   Fix the code below to log the desired output.
 */
 
 function timeOutCounter() {
+  let timing = 0;
+  function countingTime(){
+    console.log(timing);
+    return timing++;
+  }
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+    setTimeout(countingTime, i * 1000);
   }
 }
 timeOutCounter();
